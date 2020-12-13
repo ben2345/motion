3b7bed2fb73b3d6d7dc97d5d93d169c3904f2d00
Ben <benjamingerbier@gmail.com>
2020-12-13 23:48:40 +0100

    maj 3

--- scripts/timelapse.sh
+++ scripts/timelapse.sh
@@ -1,8 +1,19 @@
 
 #!/bin/bash
 
+newFilePath=/var/www/motion/files/`TZ=GMT+24 date +%Y-%m-%d`-timelapse.mpg;
+timelapseFilePath=/var/www/motion/files/timelapse/timelapse.flv
+timelapseFilePathTemp=/var/www/motion/files/timelapse/timelapse-temp.flv
+timelapseFilePathNew=/var/www/motion/files/timelapse/timelapse-new.flv
 
+if test -f "$timelapseFilePath"; then
+    if test -f "$newFilePath"; then
+        ffmpeg -i $newFilePath -filter:v "crop=470:920:0:170" $timelapseFilePathNew -y
+    fi
+    cp -v $timelapseFilePath $timelapseFilePathTemp
+    if test -f "$timelapseFilePath"; then
+        ffmpeg -i "concat:$timelapseFilePathTemp|$timelapseFilePathNew" -c copy $timelapseFilePath -y
+    fi
+fi
 
-
-# ffmpeg -i $entry -filter:v "crop=470:920:0:170" /var/www/motion/files/events/$(date +"%Y-%m-%d")/${fileName##*/}.gif
-
+rm $timelapseFilePathTemp $timelapseFilePathNew
